a9e2932f0824cd8e717863a0cf09564d8124cf15
Arjen Roodselaar <arjen@fb.com>
2014-01-13 19:18:16 -0800

    Parse AST of hello_world.go

--- src/hho.go
+++ src/hho.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"go/token"
+	"go/parser"
+	"go/ast"
+)
+
+func get_ast(name string) (fset *token.FileSet, f *ast.File, err error) {
+	fset = token.NewFileSet()
+	f, err = parser.ParseFile(fset, name, nil, parser.Mode(0))
+	return
+}
+
+func print_ast(fset *token.FileSet, f *ast.File) {
+	ast.Print(fset, f)
+}
+
+func print_hhas(fset *token.FileSet, f *ast.File) {
+}
 
 func main() {
-	fmt.Printf("Hello world!")
+	fset, f, err := get_ast("/Users/arjen/dev/hho/examples/hello_world.go")
+	if (err != nil) {
+		panic(err)
+	}
+	print_ast(fset, f)
+	print_hhas(fset, f)
 }
